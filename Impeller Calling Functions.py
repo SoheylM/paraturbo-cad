b1abe528d1f1b057e6cb8273b0fdad9a3df69f5f
Joseph Bejjani <joseph.bejjani@epfl.ch>
2023-04-27 19:00:54 +0200

    minor fix

--- Impeller Calling Functions.py
+++ Impeller Calling Functions.py
@@ -2,6 +2,12 @@ import cadquery as cq
 import os
 cwf = os.getcwd()
 
+from ocp_vscode import show, show_object, reset_show, set_port, set_defaults, get_defaults
+set_port(3939)
+
+reset_show() # use for reapeated shift-enter execution to clean object buffer
+set_defaults(axes=True, transparent=False, collapse=1, grid=(True, True, True))
+
 import sys
 sys.path.append(cwf  + '/COMP')
 
@@ -25,8 +31,8 @@ Coords_splitterblades = Imp.blades_excel('POINT_BLADES2.xls')
 Splitterblade = Imp.model_blades(Coords_splitterblades)
 Splitterblades = Imp.rotate_blade(Splitterblade)
 
-Help.assemble((Hub,Mainblades,Splitterblades))
+#Help.assemble((Hub,Mainblades,Splitterblades))
 
-#show_object(Hub)
-#show_object(Mainblades)
-#show_object(Splitterblades)
+# show_object(Hub)
+# show_object(Mainblades)
+# show_object(Splitterblades)
