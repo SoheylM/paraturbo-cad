482af7626448bead85f8d3174d8acebf73fb885b
Timothy Horvath <timothy.horvath@epfl.ch>
2023-05-10 15:43:22 +0200

    Implemented removal cylinder in takingrotor.py

--- HGJB/takingrotor.py
+++ HGJB/takingrotor.py
@@ -1,9 +1,39 @@
+import pickle
+import numpy as np
 import cadquery as cq
 import os
 from math import sin, cos, pi, tan
 import timeit
 from enum import Enum, auto
 import cq_warehouse.extensions
+
+#open file
+file = open('Element_23_05_03.pickle', 'rb')
+
+# dump info to that file
+Element = pickle.load(file)
+
+#close file
+file.close()
+
+Laenge = Element['Laenge']
+#change list into numpy array
+Laenge = np.array(Laenge)
+#change units from m to mm to avoid hollow visual effect
+
+Laenge = 1000*Laenge 
+#multiplying Laenge streeeetches the part out, so don't
+DI1 = 1000*np.array(Element['DI1'])
+DI2 = 1000*np.array(Element['DI2'])
+DI3 = 1000*np.array(Element['DI3'])
+DA1 = 1000*np.array(Element['DA1'])
+DA2 = 1000*np.array(Element['DA2'])
+DA3 = 1000*np.array(Element['DA3'])
+#print('type DI1', type(DI1))
+
+sys_pos = Element['sys_pos']
+pos_hgjb1 = sys_pos['pos_hgjb1']
+pos_hgjb2 =sys_pos['pos_hgjb2']
 
 cwf = os.getcwd().replace("\\", "/")
 
@@ -26,7 +56,7 @@ gamma_HG = 0.89 #given
 h_gr = 16 #groove depth given in micrometers
 h_rr = 9 #clearance on radiu given in micrometers
 D = 16 #on drawing [mm]
-L = 28 #on drawing [mm]
+L = 28 #length of HGJB on drawing [mm]
 L_land=L-(gamma_HG*L) #Value for CAD
 L=L+0.8 #oversized length for safety generally between 0.6 - 1
 Spiral_step = pi*D*tan(beta_HG)
@@ -34,7 +64,39 @@ Spiral_height = L/2
 a_HG = (pi*D*alpha_HG)/N_HG #mm
 a_HG_plus_b_HG = a_HG/alpha_HG #mm
 h_rr_tot = h_rr*2 #diametral clearance given in micrometers
-
 #end code adapted from Christophe's Matlab
 
+#find distance to first HGJB
+dist1 = 0
+for d in range(pos_hgjb1):
+    dist1=dist1+Laenge[d]
+#find distance to center of first HGJB
+dist1=dist1+L/2
+
+#define separation angle between grooves
+sepang=360/N_HG
+
+#length between parallelogram verticals
+LenBetwVert = L/2 - L_land/2
+
+#Angle between parallelogram diagonal and reference horizontal
+Betaprime = abs(beta_HG) - 90
+
+#gap between horizontal leaving from parallelogram lowest 
+#corner and parallelogram vertical
+
+gap = LenBetwVert*tan(Betaprime)
+
+#create removal cylinder
+CylLen = Laenge[pos_hgjb1]
+CylRadOut= DA3[pos_hgjb1]
+
+removalcylinder = cq.Solid.makeCylinder(
+      CylRadOut, CylLen, pnt=cq.Vector(0, dist1, -DA3[pos_hgjb1]*0.8), dir=cq.Vector(0, 1, 0)
+)
+
+#direction of projection 
+projection_direction = cq.Vector(0, 0, 1)
+
 show_object(Rotor)
+#show_object(removalcylinder)
