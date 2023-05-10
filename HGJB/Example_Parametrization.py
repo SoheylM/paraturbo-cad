ad95798f506fe94db95db97f4b52c500a96f0ad9
Timothy Horvath <timothy.horvath@epfl.ch>
2023-05-10 10:16:06 +0200

    Preparing to add grooves to actual rotors

--- HGJB/Example_Parametrization.py
+++ HGJB/Example_Parametrization.py
@@ -51,7 +51,7 @@ cylinder = cq.Solid.makeCylinder(
 #cylinder = cylinder.transformed(rotate=(0,60,0))
 
 removalcylinder = cq.Solid.makeCylinder(
-      CylRadOut, CylLen, pnt=cq.Vector(0, -50, -50), dir=cq.Vector(0, 1, 0)
+      CylRadOut, CylLen, pnt=cq.Vector(0, -50, -30), dir=cq.Vector(0, 1, 0)
 )
 
 #direction of projection 
@@ -109,27 +109,15 @@ parallelogram2 = (
 #       .val()
 #   )
 
-#draw second parallelogram as a 3D shape in YX plane
-# parallelogram2 = (
-#      cq.Workplane("YX", origin=(0, -35, -2*CylRadOut))
-#      #when viewed / \, y to the left and x down, z into screen
-#      #origin at upper outside corner
-#      .lineTo(30,10) #upper inside corner
-#      .lineTo(30,0) #lower inside corner
-#      .lineTo(0,-10) #lower outside corner
-#      .close()
-#      .extrude(1)
-#      .faces("<Z")
-#      .val()
-#  )
-#project first parallelogram onto cylinder
-# parallelogram1_projected = parallelogram1.projectToShape(cylinder, projection_direction)
 
-# #turn first parallelogram into 3D shape on cylinder surface
-# parallelogram1_solids = cq.Compound.makeCompound(
-#       [f.thicken(2) for f in parallelogram1_projected]
-#   )
-# parallelogram1_solids = parallelogram1_solids.cut(removalcylinder)
+#project first parallelogram onto cylinder
+parallelogram1_projected = parallelogram1.projectToShape(cylinder, projection_direction)
+
+#turn first parallelogram into 3D shape on cylinder surface
+parallelogram1_solids = cq.Compound.makeCompound(
+      [f.thicken(2) for f in parallelogram1_projected]
+  )
+parallelogram1_solids = parallelogram1_solids.cut(removalcylinder)
 
 # #project second parallelogram onto cylinder
 # parallelogram2_projected = parallelogram2.projectToShape(cylinder, projection_direction)
@@ -152,7 +140,7 @@ parallelogram2 = (
 
 #show the various objects
 if "show_object" in locals():
-     show_object(cylinder, name="cylinder_solid") #, options={"alpha": 0.8}
+     show_object(cylinder, name="cylinder_solid", options={"alpha": 0.8})
      #show_object(removalcylinder)
      show_object(parallelogram1, name="parallelogram1")
      show_object(parallelogram2, name="parallelogram2")
