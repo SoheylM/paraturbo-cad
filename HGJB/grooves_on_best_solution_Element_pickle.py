2d288ccd383bc5f237f8edfded9cfef084efc2a2
Timothy Horvath <timothy.horvath@epfl.ch>
2023-05-10 14:32:54 +0200

    Implemented code from matlab in takingrotor.py

--- HGJB/grooves_on_best_solution_Element_pickle.py
+++ HGJB/grooves_on_best_solution_Element_pickle.py
@@ -1,6 +1,12 @@
 import pickle
 import numpy as np
 import cadquery as cq
+from math import sin, cos, pi, tan
+import timeit
+from enum import Enum, auto
+import cq_warehouse.extensions
+
+#creating the rotor
 
 #open file
 file = open('best_solution_Element.pickle', 'rb')
@@ -106,8 +112,158 @@ for i in range(21):
         part1=WP.cylinder(Laenge[i+1],DA1[i+1]/2, direct=(0,0,1), angle = 360,centered =(True,True,False),combine=False).faces('>Z').hole(DI1[i+1],Laenge[i+1])
         show_object(part1)
 
-#try moving the workplane back to the front or making cylinder come back
-#try cutting the parallelogram and then filling the hole left with another cylinder
+
+
+#creating the grooves
+
+#number of units present
+units=28
+
+#calculate angle of separation based on how many units present
+sepang=360/units
+
+#Length of entire bearing along cylinder axis
+Len = Laenge[pos_hgjb1]
+
+# #Length of middle strip
+L_land = 3.08 #mm
+
+# #Distance to center of bearing along rotor axis
+for d in range(pos_hgjb1):
+    
+# DistCenter = Len/2 #+sum of all other cylinder lengths
+
+# #Outside angle between parallelogram vertical and diagonal
+# Beta = 135
+
+# #Groove height (parallelogram vertical)
+# a = 1
+
+# #length between parallelogram verticals
+# LenBetwVert = Len/2 - Lland/2
+
+# #Angle between parallelogram diagonal and reference horizontal
+# Betaprime = Beta - 90
+
+# #gap between horizontal leaving from parallelogram lowest 
+# #corner and parallelogram vertical
+
+# gap = LenBetwVert*tan(Betaprime)
+
+
+# #Length of cylinder section
+# CylLen = 100
+
+# #Outer radius of the cylinder
+# CylRadOut = 50
+
+# #cylinder to be projected onto (100 is the length)
+# cylinder = cq.Solid.makeCylinder(
+#      CylRadOut, CylLen, pnt=cq.Vector(0, -50, 0), dir=cq.Vector(0, 1, 0)
+# )
+
+# #cylinder = cylinder.transformed(rotate=(0,60,0))
+
+# removalcylinder = cq.Solid.makeCylinder(
+#       CylRadOut, CylLen, pnt=cq.Vector(0, -50, -30), dir=cq.Vector(0, 1, 0)
+# )
+
+# #direction of projection 
+# projection_direction = cq.Vector(0, 0, 1)
+
+# yp1 = DistCenter 
+# xp1 = LenBetwVert*tan(Betaprime)/2 
+# yp2 = -LenBetwVert*tan(Betaprime)
+# xp2 = -LenBetwVert
+
+# #draw first parallelogram as a 3D shape in YX plane
+# parallelogram1 = (
+#       cq.Workplane("YX", origin=((gap+a)/2, yp1-1, -2*CylRadOut))
+#       #when viewed / \, y to the right and x up, z into screen
+#       #origin at upper outside corner
+#       #points below in standard x and y coordinates
+#       .lineTo(-LenBetwVert,-gap) #upper inside corner 
+#       .lineTo(-LenBetwVert,-gap-a) #lower inside corner
+#       .lineTo(0,-a) #lower outside corner
+#       .close()
+#       .extrude(1)
+#       .faces("<Z")
+#       .val()
+#   )
+
+# parallelogram2 = (
+#       cq.Workplane("YX", origin=((gap+a)/2, -yp1+1, -2*CylRadOut))
+#       #when viewed / \, y to the right and x up, z into screen
+#       #origin at upper outside corner
+#       #points below in standard x and y coordinates
+#       .lineTo(LenBetwVert,-gap) #upper inside corner 
+#       .lineTo(LenBetwVert,-gap-a) #lower inside corner
+#       .lineTo(0,-a) #lower outside corner
+#       .close()
+#       .extrude(1)
+#       .faces("<Z")
+#       .val()
+#   )
+
+# # #height between outer top and inner top
+# # h=60
+# # #vertical height
+# # v=1
+
+# # parallelogram1 = (
+# #       cq.Workplane("YX", origin=((h+v)/2, 40, -2*CylRadOut))
+# #       #when viewed / \, y to the right and x up, z into screen
+# #       #origin at upper outside corner
+# #       .lineTo(-40,-h) #upper inside corner 
+# #       .lineTo(-40,-h-v) #lower inside corner
+# #       .lineTo(0,-v) #lower outside corner
+# #       .close()
+# #       .extrude(1)
+# #       .faces("<Z")
+# #       .val()
+# #   )
+
+
+# #project first parallelogram onto cylinder
+# parallelogram1_projected = parallelogram1.projectToShape(cylinder, projection_direction)
+
+# #turn first parallelogram into 3D shape on cylinder surface
+# parallelogram1_solids = cq.Compound.makeCompound(
+#       [f.thicken(2) for f in parallelogram1_projected]
+#   )
+# parallelogram1_solids = parallelogram1_solids.cut(removalcylinder)
+
+# # #project second parallelogram onto cylinder
+# # parallelogram2_projected = parallelogram2.projectToShape(cylinder, projection_direction)
+
+# # #turn second parallelogram into 3D shape on cylinder surface
+# # parallelogram2_solids = cq.Compound.makeCompound(
+# #      [f.thicken(2) for f in parallelogram2_projected]
+# #  )
+
+# # parallelogram2_solids = parallelogram2_solids.cut(removalcylinder)
+
+# # #removes the projected parallelogram 3D objects from the cylinder
+
+# # for i in range(units):
+# #     cylinder = cylinder.cut(parallelogram1_solids)
+# #     #cylinder = cylinder.cut(parallelogram2_solids)
+# #     cylinder = cylinder.transformed(rotate=(0,sepang,0))
+
+# # cylinder=cylinder.transformed(rotate=(0,0,0))
+
+# #show the various objects
+# if "show_object" in locals():
+#      show_object(cylinder, name="cylinder_solid", options={"alpha": 0.8})
+#      #show_object(removalcylinder)
+#      show_object(parallelogram1, name="parallelogram1")
+#      show_object(parallelogram2, name="parallelogram2")
+     
+#      #these are the two objects we are interested in
+#      #each one creates a curved parallelogram outside and
+#      #inside the cylinder; need to show both and remove excess
+#      #show_object(parallelogram1_solids, name="parallelogram1_solids")
+#      #show_object(parallelogram2_solids, name="parallelogram2_solids")
 
 
 
--- HGJB/takingrotor.py
+++ HGJB/takingrotor.py
@@ -1,5 +1,9 @@
 import cadquery as cq
 import os
+from math import sin, cos, pi, tan
+import timeit
+from enum import Enum, auto
+import cq_warehouse.extensions
 
 cwf = os.getcwd().replace("\\", "/")
 
@@ -9,6 +13,28 @@ Rotor = cq.importers.importStep(cwf + '/Rotor.stp')
 # Use this in VS Code
 # Rotor = cq.importers.importStep(cwf + '/HGJB/Rotor.stp')
 
-Rotor=Rotor.transformed(rotate=(0,0,0))
+
+Rotor = Rotor.rotate((0,0,0),(1,0,0),270)
+
+#Begin code adapted from Christophe's Matlab
+#Parameters 
+N_HG = 28 #number of grooves generally between 26 - 30
+alpha_HG = 0.68 #given
+beta_HG = -135 #given 
+beta_HG = beta_HG*pi/180
+gamma_HG = 0.89 #given
+h_gr = 16 #groove depth given in micrometers
+h_rr = 9 #clearance on radiu given in micrometers
+D = 16 #on drawing [mm]
+L = 28 #on drawing [mm]
+L_land=L-(gamma_HG*L) #Value for CAD
+L=L+0.8 #oversized length for safety generally between 0.6 - 1
+Spiral_step = pi*D*tan(beta_HG)
+Spiral_height = L/2
+a_HG = (pi*D*alpha_HG)/N_HG #mm
+a_HG_plus_b_HG = a_HG/alpha_HG #mm
+h_rr_tot = h_rr*2 #diametral clearance given in micrometers
+
+#end code adapted from Christophe's Matlab
 
 show_object(Rotor)
