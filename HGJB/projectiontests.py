4650c12f5d066a928c942803668bdb1d396ebd2f
Timothy Horvath <timothy.horvath@epfl.ch>
2023-05-11 11:17:13 +0200

    Test: Moved parallelogram inside cylinder

--- HGJB/projectiontests.py
+++ HGJB/projectiontests.py
@@ -1,3 +1,5 @@
+#adapted from May 11 version of takingrotor.py
+
 import pickle
 import numpy as np
 import cadquery as cq
@@ -35,16 +37,6 @@ sys_pos = Element['sys_pos']
 pos_hgjb1 = sys_pos['pos_hgjb1']
 pos_hgjb2 =sys_pos['pos_hgjb2']
 
-cwf = os.getcwd().replace("\\", "/")
-
-# Use this in CQ-Editor
-Rotor = cq.importers.importStep(cwf + '/Rotor.stp')
-
-# Use this in VS Code
-# Rotor = cq.importers.importStep(cwf + '/HGJB/Rotor.stp')
-
-
-Rotor = Rotor.rotate((0,0,0),(1,0,0),270)
 
 #Begin code adapted from Christophe's Matlab
 #Parameters 
@@ -113,14 +105,7 @@ removalcylinder1 = cq.Solid.makeCylinder(
       CylRadOut1*2, CylLen1, pnt=cq.Vector(0, DistCenter1+L/2, -DA3[pos_hgjb1]*0.8), dir=cq.Vector(0, -1, 0)
 )
 
-#second cylinder to be projected onto
-cylinder2 = cq.Solid.makeCylinder(
-     CylRadOut2, CylLen2+2, pnt=cq.Vector(0, DistCenter2+L/2+1, 0), dir=cq.Vector(0, -1, 0)
-)
-#second removal cylinder
-removalcylinder2 = cq.Solid.makeCylinder(
-      CylRadOut2*2, CylLen2, pnt=cq.Vector(0, DistCenter2+L/2, -DA3[pos_hgjb1]*0.8), dir=cq.Vector(0, -1, 0)
-)
+
 
 #direction of projection 
 projection_direction = cq.Vector(0, 0, 1)
@@ -133,7 +118,7 @@ xp2 = -LenBetwVert
 
 
 parallelogram1a = (
-      cq.Workplane("YX", origin=((gap+a_HG)/2, DistCenter1+L/2, -2*CylRadOut1))
+      cq.Workplane("YX", origin=((gap+a_HG)/2, DistCenter1+L/2, 0))
       #when viewed / \, y to the right and x up, z into screen
       #origin at upper outside corner
       #points below in standard x and y coordinates
@@ -146,48 +131,6 @@ parallelogram1a = (
       .val()
   )
 
-      #when viewed / \, y to the right and x up, z into screen
-parallelogram2a = (
-      cq.Workplane("YX", origin=((gap+a_HG)/2, DistCenter1-L/2, -2*CylRadOut1))
-      #when viewed / \, y to the right and x up, z into screen
-      #origin at upper outside corner
-      #points below in standard x and y coordinates
-      .lineTo(LenBetwVert,-gap) #upper inside corner 
-      .lineTo(LenBetwVert,-gap-a_HG) #lower inside corner
-      .lineTo(0,-a_HG) #lower outside corner
-      .close()
-      .extrude(1)
-      .faces("<Z")
-      .val()
-  )
-
-parallelogram1b = (
-      cq.Workplane("YX", origin=((gap+a_HG)/2, DistCenter2+L/2, -2*CylRadOut2))
-      #when viewed / \, y to the right and x up, z into screen
-      #origin at upper outside corner
-      #points below in standard x and y coordinates
-      .lineTo(-LenBetwVert,-gap) #upper inside corner 
-      .lineTo(-LenBetwVert,-gap-a_HG) #lower inside corner
-      .lineTo(0,-a_HG) #lower outside corner
-      .close()
-      .extrude(1)
-      .faces("<Z")
-      .val()
-  )
-
-parallelogram2b = (
-      cq.Workplane("YX", origin=((gap+a_HG)/2, DistCenter2-L/2, -2*CylRadOut2))
-      #when viewed / \, y to the right and x up, z into screen
-      #origin at upper outside corner
-      #points below in standard x and y coordinates
-      .lineTo(LenBetwVert,-gap) #upper inside corner 
-      .lineTo(LenBetwVert,-gap-a_HG) #lower inside corner
-      .lineTo(0,-a_HG) #lower outside corner
-      .close()
-      .extrude(1)
-      .faces("<Z")
-      .val()
-  )
 
 #fist HGJB
 #project first parallelogram onto cylinder
@@ -195,58 +138,15 @@ parallelogram1a_projected = parallelogram1a.projectToShape(cylinder1, projection
 
 #turn first parallelogram into 3D shape on cylinder surface
 parallelogram1a_solids = cq.Compound.makeCompound(
-      [f.thicken(h_gr/1000) for f in parallelogram1a_projected]
+      [f.thicken(1) for f in parallelogram1a_projected]
   )
-parallelogram1a_solids = parallelogram1a_solids.cut(removalcylinder1)
-
-#project second parallelogram onto cylinder
-parallelogram2a_projected = parallelogram2a.projectToShape(cylinder1, projection_direction)
-
-#turn first parallelogram into 3D shape on cylinder surface
-parallelogram2a_solids = cq.Compound.makeCompound(
-      [f.thicken(h_gr/1000) for f in parallelogram2a_projected]
-  )
-parallelogram2a_solids = parallelogram2a_solids.cut(removalcylinder1)
+#parallelogram1a_solids = parallelogram1a_solids.cut(removalcylinder1)
 
 
-#second HGJB
-#project first parallelogram onto cylinder
-parallelogram1b_projected = parallelogram1b.projectToShape(cylinder2, projection_direction)
+cylinder1 = cylinder1.cut(parallelogram1a_solids)
 
-#turn first parallelogram into 3D shape on cylinder surface
-parallelogram1b_solids = cq.Compound.makeCompound(
-      [f.thicken(h_gr/1000) for f in parallelogram1b_projected]
-  )
-parallelogram1b_solids = parallelogram1b_solids.cut(removalcylinder2)
 
-#project second parallelogram onto cylinder
-parallelogram2b_projected = parallelogram2b.projectToShape(cylinder2, projection_direction)
-
-#turn first parallelogram into 3D shape on cylinder surface
-parallelogram2b_solids = cq.Compound.makeCompound(
-      [f.thicken(h_gr/1000) for f in parallelogram2b_projected]
-  )
-parallelogram2b_solids = parallelogram2b_solids.cut(removalcylinder2)
-
-# for i in range(N_HG):
-#     cylinder = cylinder.cut(parallelogram1_solids)
-#     #cylinder = cylinder.cut(parallelogram2_solids)
-#     cylinder = cylinder.transformed(rotate=(0,sepang,0))
-
-for i in range(N_HG):
-    Rotor = Rotor.cut(parallelogram1a_solids)
-    Rotor = Rotor.cut(parallelogram2a_solids)
-    Rotor = Rotor.cut(parallelogram1b_solids)
-    Rotor = Rotor.cut(parallelogram2b_solids)
-    Rotor = Rotor.rotate((0,0,0),(0,1,0),sepang)
-
-Rotor = Rotor.rotate((0,0,0),(1,0,0),-270)
-
-show_object(Rotor)
 #show_object(removalcylinder1)
-#show_object(cylinder)
-# show_object(parallelogram1a, name="parallelogram1a")
-# show_object(parallelogram2a, name="parallelogram2a")
-# show_object(parallelogram1b, name="parallelogram1b")
-# show_object(parallelogram2b, name="parallelogram2b")
-#show_object(parallelogram1a_solids, name="parallelogram1a_solids")
+show_object(cylinder1, options={"alpha": 0.8})
+show_object(parallelogram1a, name="parallelogram1a")
+show_object(parallelogram1a_solids, name="parallelogram1a_solids")
