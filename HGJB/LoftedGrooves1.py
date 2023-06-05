a477f25912f606659b07faa301bd63cf72a0bc28
Timothy Horvath <timothy.horvath@epfl.ch>
2023-06-05 19:50:57 +0200

    Point missing creates wonky shape

--- HGJB/LoftedGrooves1.py
+++ HGJB/LoftedGrooves1.py
@@ -11,7 +11,7 @@ import cq_warehouse.extensions
 from cadquery import exporters
 
 #open file
-file = open('Element_23_06_05.pickle', 'rb')
+file = open('Element_23_06_05_v2.pickle', 'rb')
 
 # dump info to that file
 Element = pickle.load(file)
@@ -112,25 +112,28 @@ gap_spiral = (gap*Spiral_step)/LenBetwVert
 CylLen1 = Laenge[pos_hgjb1]
 CylRadOut1= DA3[pos_hgjb1]/2 #1.01*
 
-# hgjb1_x_first = Element['parameters']['hgjb1']['x_first_curve']
-# hgjb1_y_first = Element['parameters']['hgjb1']['y_first_curve']
-# hgjb1_z_first = Element['parameters']['hgjb1']['z_first_curve']
+hgjb1_x_first = Element['parameters']['hgjb1']['x_first_curve']
+hgjb1_y_first = Element['parameters']['hgjb1']['y_first_curve']
+hgjb1_z_first = Element['parameters']['hgjb1']['z_first_curve']
 
-hgjb1_x_first = [-1, 0, 1, 1, 1, 0, -1, -1]
-hgjb1_y_first = [2, 1, 1, 1, 1, 1, 2, 2]
-hgjb1_z_first = [3, 3, 2, 1, 0, 1, 2, 3]
+# hgjb1_x_first = [-1, 0, 1, 1, 1, 0, -1, -1]
+# hgjb1_y_first = [2, 1, 1, 1, 1, 1, 2, 2]
+# hgjb1_z_first = [3, 3, 2, 1, 0, 1, 2, 3]
 
 coords_hgjb1_first = list(zip(hgjb1_x_first, hgjb1_y_first, hgjb1_z_first))
-
-hgjb1_x_second = [-1/2, 0, 1/2, 1/2, 1/2, 0, -1/2, -1/2]
-hgjb1_y_second = [2/2, 1/2, 1/2, 1/2, 1/2, 1/2, 2/2, 2/2]
-hgjb1_z_second = [3, 3, 2, 1, 0, 1, 2, 3]
+# Adding the first point of the curve to the end of its point array to close it
+# coords_hgjb1_first.append(coords_hgjb1_first[0])
 
 
+# hgjb1_x_second = [-1/2, 0, 1/2, 1/2, 1/2, 0, -1/2, -1/2]
+# hgjb1_y_second = [2/2, 1/2, 1/2, 1/2, 1/2, 1/2, 2/2, 2/2]
+# hgjb1_z_second = [3, 3, 2, 1, 0, 1, 2, 3]
 
-# hgjb1_x_second = Element['parameters']['hgjb1']['x_second_curve']
-# hgjb1_y_second = Element['parameters']['hgjb1']['y_second_curve']
-# hgjb1_z_second = Element['parameters']['hgjb1']['z_second_curve']
+
+
+hgjb1_x_second = Element['parameters']['hgjb1']['x_second_curve']
+hgjb1_y_second = Element['parameters']['hgjb1']['y_second_curve']
+hgjb1_z_second = Element['parameters']['hgjb1']['z_second_curve']
 coords_hgjb1_second = list(zip(hgjb1_x_second, hgjb1_y_second, hgjb1_z_second))
 
 hgjb2_x_first = Element['parameters']['hgjb2']['x_first_curve']
@@ -143,55 +146,70 @@ hgjb2_y_second = Element['parameters']['hgjb2']['y_second_curve']
 hgjb2_z_second = Element['parameters']['hgjb2']['z_second_curve']
 coords_hgjb2_second = list(zip(hgjb2_x_second, hgjb2_y_second, hgjb2_z_second))
 
+
+
 points = 50
-edge_hgjb1_first = cq.Edge.makeSpline([cq.Vector(p) for p in coords_hgjb1_first][0:-1]).close()
+edge_hgjb1_first_1 = cq.Edge.makeSpline([cq.Vector(p) for p in coords_hgjb1_first][0:50]).clean()
+edge_hgjb1_first_2 = cq.Edge.makeSpline([cq.Vector(p) for p in coords_hgjb1_first][51:100]).clean()
+edge_hgjb1_first_3 = cq.Edge.makeSpline([cq.Vector(p) for p in coords_hgjb1_first][100:150]).clean()
+edge_hgjb1_first_4 = cq.Edge.makeSpline([cq.Vector(p) for p in coords_hgjb1_first][151:200]).clean()
+edge_hgjb1_first = edge_hgjb1_first_1.fuse(edge_hgjb1_first_2).fix()
+edge_hgjb1_first = edge_hgjb1_first.fuse(edge_hgjb1_first_3).fix()
+edge_hgjb1_first = edge_hgjb1_first.fuse(edge_hgjb1_first_4).fix()
 show_object(edge_hgjb1_first)
-edge_hgjb1_second = cq.Edge.makeSpline([cq.Vector(p) for p in coords_hgjb1_second][0:-1]).close()
-show_object(edge_hgjb1_second)
+
+# show_object(edge_hgjb1_first_1)
+# show_object(edge_hgjb1_first_2)
+# show_object(edge_hgjb1_first_3)
+# show_object(edge_hgjb1_first_4)
+
+# edge_hgjb1_second = cq.Edge.makeSpline([cq.Vector(p) for p in coords_hgjb1_second][0:points]).close()
+# show_object(edge_hgjb1_second)
 # edge_hgjb2_first = cq.Edge.makeSpline([cq.Vector(p) for p in coords_hgjb2_first][0:points]).close()
 # show_object(edge_hgjb2_first)
 # edge_hgjb2_second = cq.Edge.makeSpline([cq.Vector(p) for p in coords_hgjb2_second][0:points]).close()
 # show_object(edge_hgjb2_second)
 
 face_hgjb1_first = cq.Face.makeNSidedSurface([edge_hgjb1_first],[])
-face_hgjb1_second = cq.Face.makeNSidedSurface([edge_hgjb1_second],[])
+show_object(face_hgjb1_first)
+# face_hgjb1_second = cq.Face.makeNSidedSurface([edge_hgjb1_second],[])
 # face_hgjb2_first = cq.Face.makeNSidedSurface([edge_hgjb2_first],[])
 # face_hgjb2_second = cq.Face.makeNSidedSurface([edge_hgjb2_second ],[])
 
-loft_hgjb1 = cq.Solid.makeLoft([edge_hgjb1_first, edge_hgjb1_second],True)
+# loft_hgjb1 = cq.Solid.makeLoft([edge_hgjb1_first, edge_hgjb1_second],True)
 # loft_hgjb2 = cq.Solid.makeLoft([edge_hgjb2_first, edge_hgjb2_second],True)
 
-shell_hgjb1 = cq.Shell.makeShell([face_hgjb1_first, face_hgjb1_second, loft_hgjb1]).fix()
+# shell_hgjb1 = cq.Shell.makeShell([face_hgjb1_first, face_hgjb1_second, loft_hgjb1]).fix()
 # shell_hgjb2 = cq.Shell.makeShell([face_hgjb2_first, face_hgjb2_second, loft_hgjb2]).fix()
 
-#calculate turn angle in radians
-radang = (gap/CylRadOut1)#gap/CylRadOut1 #gap_spiral/(pi*D)# Betaprime #gap_spiral/pi*D #gap/CylRadOut1
+# #calculate turn angle in radians
+# radang = (gap/CylRadOut1)#gap/CylRadOut1 #gap_spiral/(pi*D)# Betaprime #gap_spiral/pi*D #gap/CylRadOut1
 
-#convert to degrees
-rotang = radang*180/pi 
+# #convert to degrees
+# rotang = radang*180/pi 
 
-solid_hgjb1 ={}
-solid_hgjb1[0] = cq.Solid.makeSolid(shell_hgjb1)
-solid_hgjb1[0] = solid_hgjb1[0].transformed((0,0,0),(0,-7.5,20))
-show_object(solid_hgjb1[0])
+# solid_hgjb1 ={}
+# solid_hgjb1[0] = cq.Solid.makeSolid(shell_hgjb1)
+# solid_hgjb1[0] = solid_hgjb1[0].transformed((0,0,0),(0,-7.5,10))
+# show_object(solid_hgjb1[0])
 
 # solid_hgjb2 ={}
 # solid_hgjb2 [0] = cq.Solid.makeSolid(shell_hgjb2)
 
 
-for i in range(0,28):
-    solid_hgjb1[i+1] = solid_hgjb1[i].transformed ((0 ,0 ,sepang))
-    #show_object(solid_hgjb1[i+1])
-    Rotor = Rotor.cut(solid_hgjb1[i+1])
+# for i in range(0,28):
+#     solid_hgjb1[i+1] = solid_hgjb1[i].transformed ((0 ,0 ,sepang))
+#     #show_object(solid_hgjb1[i+1])
+#     #Rotor = Rotor.cut(solid_hgjb1[i+1])
     
-    # solid_hgjb2[i+1] = solid_hgjb2[i].transformed ((0 ,sepang ,0))
-    # Rotor = Rotor.cut(solid_hgjb2[i+1])
+#     # solid_hgjb2[i+1] = solid_hgjb2[i].transformed ((0 ,sepang ,0))
+#     # Rotor = Rotor.cut(solid_hgjb2[i+1])
     
-    # solid_hgjb1m[i+1] = solid_hgjb1m[i].transformed ((0 ,sepang ,0))
-    # Rotor = Rotor.cut(solid_hgjb1m[i+1])
+#     # solid_hgjb1m[i+1] = solid_hgjb1m[i].transformed ((0 ,sepang ,0))
+#     # Rotor = Rotor.cut(solid_hgjb1m[i+1])
     
 
-show_object(Rotor)
+#show_object(Rotor)
 
 #cq.exporters.export(cylinder1, "HGJBonRotor_Element_23_08_19.step")
 
